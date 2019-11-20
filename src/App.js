08ebbd749dcd42084ab97aa9bff61cab1e2a8b47
Thanh <thanh-le@hotmail.de>
2019-11-20 18:33:59 +0100

    post data

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const w = "             W \n            WWW\n            WWW\n           WWWWW\n     W     WWWWW     W\n     WWW   WWWWW   WWW\n      WWW  WWWWW  WWW\n       WWW  WWW  WWW\n        WWW WWW WWW\n          WWWWWWW\n       WWWW  |  WWWW\n             |\n             |\n"
 
-    const [text, setText] = useState("");
+  const [text, setText] = useState("");
+
+ 
   return (
     <div className="App">
         <form method="POST" action="/">
-{/*
-            <input align={"top"} className={"textField"} name="text" type="text" value={text} placeholder="Hier könnte ihre Werbung stehen:" onChange={(e) => setText(e.target.value)} />
-*/}
-            <textarea wrap={"hard"} cols={"30"} className={"textField"} value={text} onChange={(e) => setText(e.target.value)} />
+            <textarea name ={"text"} wrap={"hard"} cols={"30"} className={"textField"} value={text} onChange={(e) => setText(e.target.value)} placeholder={"Teile uns etwas mit... \n\n" + w + "\n\nSei kreativ :) \n\n ┏(-_-)┛┗(-_-﻿ )┓┗(-_-)┛"} />
             <input className={"inputButton"} type="submit" value="submit" />
         </form>
     </div>
