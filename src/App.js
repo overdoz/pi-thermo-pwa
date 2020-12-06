7204554054cb40420683617367282fb03d0b85b0
overdoz <thanh-le@hotmail.de>
2020-12-07 00:19:41 +0100

    css refactor

--- src/App.js
+++ src/App.js
@@ -11,8 +11,6 @@ class App extends Component {
             name: "Stranger",
             names: ["Annalena", "Mary", "Sepp", "Thanh", "Stranger"]
         }
-
-
     }
 
     componentDidMount() {
